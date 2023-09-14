0507b359f5769d6f30709bfcadf1e18697da1385
shivam <shivam@DESKTOP-BT1O840>
2023-09-14 09:10:41 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+class Person
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+
+    public string FullUpperName
+    {
+        get { return $"{FirstName} {LastName}".ToUpper(); }
+    }
+
+    public Person(string firstName, string lastName)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Person person = new Person("Shivam", "Saini");
+
+        string fullNameUpper = person.FullUpperName;
+        Console.WriteLine("Full Name (Uppercase): " + fullNameUpper);
+
+        Console.ReadLine();
+    }
+}
